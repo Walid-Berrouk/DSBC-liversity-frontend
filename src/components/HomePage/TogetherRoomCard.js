3e95627f7c9f4b9dfb5dcdc6cc71f99b0727cdaf
Abderaouf Louggani <ja_louggani@esi.dz>
2022-08-08 17:31:22 +0100

    together rooms

---
+++ src/components/HomePage/TogetherRoomCard.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { BsCameraVideoOff } from "react-icons/bs";
+import { BiMicrophoneOff } from "react-icons/bi";
+import { IoMdChatboxes } from "react-icons/io";
+import { MdGroups } from "react-icons/md";
+import { AiOutlineRight } from "react-icons/ai";
+
+export default function TogetherRoomCard({ room }) {
+  return (
+    <div className="h-72 w-72 bg-white my-8 mx-auto rounded-3xl overflow-hidden relative">
+      <div className="absolute top-1/3 -translate-y-1/2 left-6">
+        <img className="rounded-full" src={room.picture} alt="" />
+      </div>
+      <div className="absolute top-1/3 -translate-y-1/2 right-6 bg-white flex items-center gap-x-2 px-2 rounded-full shadow-md">
+        <div>
+          <MdGroups className="text-pink-500 text-xl" />
+        </div>
+        <div>
+          <span className="font-semibold">{room.actualNumber}</span>/
+          {room.maxNumber}
+        </div>
+      </div>
+      <div className="h-1/3 w-full bg-pink-500 px-4 py-2">
+        <div className="flex justify-between">
+          <div className="bg-white flex gap-x-1 items-center px-2 rounded-full">
+            <div className="bg-green-500 w-3 h-3 rounded-full" />
+            <div className="text-green-500">LIVE</div>
+          </div>
+          <div className="text-white text-xl flex gap-x-4">
+            <button>
+              <BsCameraVideoOff />
+            </button>
+            <button>
+              <BiMicrophoneOff />
+            </button>
+            <button>
+              <IoMdChatboxes />
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-start my-2">
+          <h4 className="text-white text-lg">{room.name}</h4>
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-8 px-2">
+        {room.categories.map((category) => (
+          <div className="bg-gray-200 rounded-3xl font-semibold mx-2 my-1 px-2">
+            {category}
+          </div>
+        ))}
+      </div>
+      <p className="px-4 my-1 text-start text-sm">{room.description}</p>
+      <div className="flex justify-end">
+        <button className="bg-pink-500 py-1 px-4 mx-4 my-2 text-white font-semibold items-center flex rounded-lg gap-x-2">
+          <div className="">Lunch solo session</div>
+          <AiOutlineRight className="text-xl" />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,111 @@ import Rooms from "../components/HomePage/Rooms";
 import Sidebar from "../components/HomePage/Sidebar";
 import SoloRoomCard from "../components/HomePage/SoloRoomCard";
 import Top from "../components/HomePage/Top";
+import TogetherRoomCard from "../components/HomePage/TogetherRoomCard";
 
 export default function Home() {
   const [name, setName] = useState("Full name");
   const [picture, setPicture] = useState("https://placehold.co/64x64.png");
   const [type, setType] = useState("Student");
   const [connected, setConnected] = useState(0);
+  const rooms = [
+    {
+      id: 1,
+      name: "Competitive exam",
+      picture: "https://placehold.co/40x40.png",
+      maxNumber: 25,
+      actualNumber: 20,
+      categories: [
+        "Exams",
+        "University",
+        "Study",
+        "1st year students",
+        "Focus",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 2,
+      name: "Competitive exam",
+      picture: "https://placehold.co/40x40.png",
+      maxNumber: 25,
+      actualNumber: 20,
+      categories: [
+        "Exams",
+        "University",
+        "Study",
+        "1st year students",
+        "Focus",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 3,
+      name: "Competitive exam",
+      picture: "https://placehold.co/40x40.png",
+      maxNumber: 25,
+      actualNumber: 20,
+      categories: [
+        "Exams",
+        "University",
+        "Study",
+        "1st year students",
+        "Focus",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 4,
+      name: "Competitive exam",
+      picture: "https://placehold.co/40x40.png",
+      maxNumber: 25,
+      actualNumber: 20,
+      categories: [
+        "Exams",
+        "University",
+        "Study",
+        "1st year students",
+        "Focus",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 5,
+      name: "Competitive exam",
+      picture: "https://placehold.co/40x40.png",
+      maxNumber: 25,
+      actualNumber: 20,
+      categories: [
+        "Exams",
+        "University",
+        "Study",
+        "1st year students",
+        "Focus",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 6,
+      name: "Competitive exam",
+      picture: "https://placehold.co/40x40.png",
+      maxNumber: 25,
+      actualNumber: 20,
+      categories: [
+        "Exams",
+        "University",
+        "Study",
+        "1st year students",
+        "Focus",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+  ];
 
   return (
     <div className="flex">
@@ -18,10 +117,18 @@ export default function Home() {
         <Top name={name} picture={picture} type={type} />
         <div className="px-24">
           <Enjoy connected={connected} />
-          <Rooms/>
+          <Rooms />
           <div className="flex flex-col items-start my-8">
             <h3 className="text-2xl font-semibold">Solo study rooms</h3>
-            <SoloRoomCard/>
+            <SoloRoomCard />
+          </div>
+          <div className="flex flex-col items-start my-8">
+            <h3 className="text-2xl font-semibold">Study together rooms</h3>
+            <div className="flex flex-wrap gap-x-2">
+              {rooms.map((room) => (
+                <TogetherRoomCard key={room.id} room={room}/>
+              ))}
+            </div>
           </div>
         </div>
       </div>
